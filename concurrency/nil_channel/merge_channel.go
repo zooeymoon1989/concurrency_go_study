6a2e54f2e91968d167b1ed55106fd9ffde5312fc
zooeymoon <kimmi-moon@163.com>
2020-03-24 08:23:57 +0800

    两种方法合并channel

--- concurrency/nil_channel/merge_channel.go
+++ concurrency/nil_channel/merge_channel.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"reflect"
 	"sync"
 	"time"
 )
@@ -14,7 +15,7 @@ func main() {
 	c := asMergeChan(20, 21, 22, 23, 24, 25, 26, 27, 28, 29)
 	d := asMergeChan(30, 31, 32, 33, 34, 35, 36, 37, 38, 39)
 
-	for v := range mergeChan(a, b, c, d) {
+	for v := range mergeReflect(a, b, c,d) {
 		fmt.Println(v)
 	}
 
@@ -26,9 +27,9 @@ func mergeChan(chans ...<-chan int) <-chan int {
 	go func() {
 		var wg sync.WaitGroup
 		wg.Add(len(chans))
-
+		defer close(out)
 		for _, c := range chans {
-			go func(<-chan int) {
+			go func(c <-chan int) {
 				for v := range c {
 					out <- v
 				}
@@ -37,10 +38,43 @@ func mergeChan(chans ...<-chan int) <-chan int {
 		}
 
 		wg.Wait()
-		close(out)
+
 	}()
 
 	return out
+}
+
+func mergeReflect(chans ...<-chan int) <-chan int {
+
+	out := make(chan int)
+
+	go func() {
+		defer close(out)
+		var cases []reflect.SelectCase
+		for _ , c := range chans {
+			cases = append(cases , reflect.SelectCase{
+				Dir:  reflect.SelectRecv,
+				Chan: reflect.ValueOf(c),
+			})
+		}
+
+		for len(cases) > 0 {
+
+			i , v , ok := reflect.Select(cases)
+			if !ok {
+				cases = append(cases[:i],cases[i+1:]...)
+				continue
+			}
+
+			out <- v.Interface().(int)
+
+		}
+	}()
+
+
+	return out
+
+
 }
 
 func asMergeChan(vs ...int) <-chan int {
