c700927b031ad50738a30721b5185f3b649491d7
zooeymoon <kimmi-moon@163.com>
2020-03-26 01:47:15 +0800

    测试

--- concurrency/merge_channel/merge_channel_test.go
+++ concurrency/merge_channel/merge_channel_test.go
@@ -5,24 +5,41 @@ import (
 	"testing"
 )
 
-func BenchmarkMergeChan(b *testing.B)  {
+var funcs = []struct {
+	name string
+	f    func(...<-chan int) <-chan int
+}{
+	{
+		"goroutines",
+		MergeChan,
+	}, {
+		"reflection",
+		MergeReflect,
+	},
+}
 
-	for i := 1 ; i <= 1024 ; i *= 2 {
-		b.Run(fmt.Sprintf("%d",i), func(b *testing.B) {
-			c := MergeReflect(AsMergeChan(1,2,3), AsMergeChan(4,5,6), AsMergeChan(7,8,9))
-			seen:=make(map[int]bool)
-			for v := range c {
-				if seen[v] {
-					b.Errorf("saw %d at least twice",v)
-				}
-				seen[v] = true
-			}
+func BenchmarkMergeChan(b *testing.B) {
 
-			for i := 1;i<=9;i++{
-				if !seen[i] {
-					b.Errorf("didn't see %d",i)
+	for _ , v := range funcs {
+		for n := 1; n <= 1024; n *= 2 {
+
+			b.Run(fmt.Sprintf("%s/%d", v.name , n), func(b *testing.B) {
+				chans := make([]<-chan int, n)
+				for i := 0; i < b.N; i++ {
+
+					for i := range chans {
+						chans[i] = AsMergeChan(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+					}
+
+					c := v.f(chans...)
+
+					for range c {
+
+					}
+
 				}
-			}
-		})
+
+			})
+		}
 	}
 }
