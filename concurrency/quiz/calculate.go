0c721b7132fe451b653f4f393eb2fcf3c47966fb
李文强 <wenqiang.li@wenba100.com>
2020-03-26 13:58:25 +0800

    添加计算pi的方法，n越大，精度越大

---
+++ concurrency/quiz/calculate.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"strconv"
+)
+
+func main() {
+	fmt.Println(calculatePi(10000000))
+}
+
+func calculatePi(n int) float64 {
+	var squareTotal, circleTotal int
+	for i := 0; i < n; i++ {
+		x := rand.Float64()
+		y := rand.Float64()
+
+		if math.Pow(x, float64(2))+math.Pow(y, float64(2)) < 1 {
+			circleTotal++
+		}
+		squareTotal++
+	}
+	value, _ := strconv.ParseFloat(fmt.Sprintf("%1.10f", float64(4 * circleTotal) / float64(squareTotal)), 64)
+	return value
+}
